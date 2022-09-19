a389d8184903b77341620cd4270f32d756c65c3e
simonustc <100547917+simonustc@users.noreply.github.com>
2022-09-19 10:14:41 +0800

    Delete origin_PPL+CRI directory

--- origin_PPL+CRI/loss/__init__.py
+++
@@ -1,8 +0,0 @@
-'''
-from .loss_old import (
-    FocalLoss, CrossEntropyLabelSmooth, CrossEntropy,
-    CostSensitiveCE, CDT, ClassBalanceCE, ClassBalanceFocal,
-    BalancedSoftmaxCE, SEQL, LDAMLoss
-)
-'''
-from .loss import CrossEntropy, LDAMLoss, FocalLoss, LOWLoss, GHMCLoss, CCELoss, CRILoss
